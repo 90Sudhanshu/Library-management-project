bd0313c4f8889cb96c3ef6444cf26d1a5eebb8a4
Sudhanshu <kesharwani.sudhanshu1990@gmail.com>
2023-06-19 20:51:55 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Library Management", layout= "centered")
+
+col1, col2 = st.columns([1, 2])
+
+col1.text("LOGO")
+col2.markdown("**The PD Library**")
